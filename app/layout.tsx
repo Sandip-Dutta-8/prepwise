581903fc618e59e6a1dc0cfe7d6c6f939c59b23d
Sandip_Dutta_8 <ayandutta2005@gmail.com>
2026-07-11 09:58:22 +0530

    Project initialized in Next.js and Shadcnui

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { DM_Sans, Lora } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const lora = Lora({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  style: ["normal", "italic"],
+  variable: "--font-serif",
+});
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Prepwise",
+  description: "An AI powered interview platform.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body className={`${lora.variable} ${dmSans.variable} font-sans`} cz-shortcut-listen="true">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+
+          <main className="min-h-screen ">
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
